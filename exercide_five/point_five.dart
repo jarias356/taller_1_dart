5c1bc301b4af699c2ea400242afc693c1ab0c534
jhonatan.arias <jhonatan.arias@onoff.com.co>
2024-03-08 15:01:01 -0500

    Set all in arespective file

--- exercide_five/point_five.dart
+++ exercide_five/point_five.dart
@@ -1,5 +1,3 @@
-import 'Product.dart';
-
 /** Define una clase en Dart llamada "Producto" con atributos como nombre, precio y
 cantidad en inventario. Crea objetos de esta clase, realiza operaciones como la
 compra, venta y mostrar los detalles actualizados de los productos. Es necesario
@@ -22,5 +20,72 @@ void firstProduct() {
   apple.doSale(11);
   apple.doSale(10);
   apple.doSale(1);
+
+}
+
+class Pair {
+  late bool first;
+  late String second;
+
+  Pair({required bool first, required String second}) {
+    this.first = first;
+    this.second = second;
+
+  }
+
+}
+
+class Product {
+  String _productName = "";
+  double _productPrice = 0.0;
+  int _productQuantity = 0;
+
+  Product({required String name, required double price, required int quantity}) {
+    _productName = name;
+    if (price > 0) {
+      _productPrice = price;
+
+    } 
+    if (quantity > 0) {
+      _productQuantity = quantity; 
+
+    }
+    
+
+  }
+
+  int getProductQuantity() => _productQuantity;
+  double getProductPrice() => _productPrice;
+  String getProductName() => _productName;
+
+  Pair _productQuantityValidBySale(int quantityBySale) {
+    if (_productQuantity <= 0) return Pair(first: false, second: "Stock en 0 o negativo");
+
+    if (quantityBySale <= 0) return Pair(first: false, second: "No pueded vengar cantidades negativas");
+    if (_productQuantity < quantityBySale) return Pair(first: false, second: "La cantidad a vender es mayor al stock disponible");
+
+    return Pair(first: true, second: "Puede continuar con la venta");
+    
+
+  }
+
+  void doSale(int quantityBySale) {
+    var response = _productQuantityValidBySale(quantityBySale);
+    if (!response.first) {
+      print(response.second);
+      return;
+    }
+    _productQuantity = _productQuantity - quantityBySale;
+
+    print("Se vendieron $quantityBySale del producto $_productName, quedando un total de $_productQuantity de stock disponoble.");
+
+  }
+
+   void doShop(int quantityByShop) {
+    _productQuantity = _productQuantity + quantityByShop;
+
+    print("Se vendieron $quantityByShop del producto $_productName, quedando un total de $_productQuantity de stock disponoble.");
+
+  }
 
 }
--- exercise_one/point_one.dart
+++ exercise_one/point_one.dart
@@ -1,5 +1,3 @@
-import 'Person.dart';
-
 /** Escribir un programa que defina una clase Persona con los atributos nombre, edad y
 género, y un método presentarse que imprima en la consola (haciendo uso de 1 solo
 print) una breve descripción de la persona. **/
@@ -8,5 +6,25 @@ void main() {
   Person person = new Person(name: "Jhoto", genre: "Masculino", age: 22);
 
   person.appear();
+
+}
+
+class Person {
+  late String name;
+  late String genre;
+  late int age;
+
+  Person({required String name, required String genre, required int age}) {
+    this.name = name;
+    this.genre = genre;
+    this.age = age;
+
+  }
+
+  void appear() {
+    print("\nHolo yo soy $name, tengo $age de edad, y mi genero es $genre.");
+
+  }
+
 
 }
